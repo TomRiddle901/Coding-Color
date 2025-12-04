af66fb31f41080b6732361b0ff00cf97f3e72c97
TommyMangia <tommaso.mangialardo@hotmail.com>
2025-12-04 11:08:27 +0100

    Inner HTML on elabora

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ console.log("Inizio Esecuzione Programma");
 function elabora(){
     let numero = document.getElementById("inNumber").value;
     if (numero >= 0 && numero <= 255){
-        alert(numero)
+        document.getElementById("outText").innerHTML = "Hai inserito il numero " + numero;
     } else{
-        alert("Numero non valido!");
+        document.getElementById("error").innerHTML = "Errore: il numero " + numero + " non rispetta i criteri!";
     }
 }
 
