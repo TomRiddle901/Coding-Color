f6eee0d8a3bc4e51fac359475071ecc07626c4d1
TommyMangia <tommaso.mangialardo@hotmail.com>
2025-12-04 10:46:54 +0100

    Function Elabora

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Inizio Esecuzione");
+console.log("Inizio Esecuzione Programma");
+
+function elabora(){
+    let numero = document.getElementById("idNumber").value;
+    alert(numero);
+}
+
